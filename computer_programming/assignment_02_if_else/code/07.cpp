ab35c8a7d7e5fcc8a5690ab7cc571c79b284cc48
moeenn <moeen.v8@gmail.com>
2019-04-29 11:14:56 +0500

    04/29/2019 11:14:56 AM

---
+++ computer_programming/assignment_02_if_else/code/07.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+	// ask user for two numbers
+	cout << "Please enter two numbers: ";
+	float num1, num2;
+	cin >> num1 >> num2;
+
+	// check which number is larger
+	( num1 > num2 ) ? cout << "Number 1 (" << num1 << ") is larger than Number 2 (" << num2 << ")" : cout << "Number 2 (" << num2 << ") is larger than Number 1 (" << num1 << ")";
+
+	return 0;
+}
