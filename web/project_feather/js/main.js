03ea021d3ec2eca0d3a1e54cc22ed675a763865c
moeenn <moeen.v8@gmail.com>
2020-08-26 17:07:39 +0500

    August 26, 2020 - 05:07:39 PM

--- web/project_feather/js/main.js
+++ web/project_feather/js/main.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", main);
 function main() {
   toggleNavLinks();
   toggleFAQEntries();
+  quantityCounter();
+  productPageTabs();
 }
 
 // - misc. utility functions -----------------------------------
@@ -54,4 +56,123 @@ function toggleNavLinks() {
   };
 
   expandButton.addEventListener("click", clickHandler);
+}
+
+// - product description & order page ----------------------------
+function quantityCounter() {
+  incrementQuantityCounter();
+  decrementQuantityCounter();
+}
+
+function incrementQuantityCounter() {
+  const incrementButton = $(".__quantity_increment");
+  if (!incrementButton) {
+    console.log("Quantity Increment button not found");
+    return;
+  }
+
+  incrementButton.addEventListener("click", () => {
+    console.log("Increment Action");
+    let quantityValue = $(".__quantity_value");
+
+    if (!quantityValue) {
+      console.log("Quantity value not found");
+      return;
+    }
+
+    const newValue = parseInt(quantityValue.innerText) + 1;
+    quantityValue.innerText = newValue;
+  });
+}
+
+function decrementQuantityCounter() {
+  const decrementButton = $(".__quantity_decrement");
+  if (!decrementButton) {
+    console.log("Quantity Decrement button not found");
+    return;
+  }
+
+  decrementButton.addEventListener("click", () => {
+    console.log("Decrement Action");
+    let quantityValue = $(".__quantity_value");
+
+    if (!quantityValue) {
+      console.log("Quantity value not found");
+      return;
+    }
+
+    const newValue = parseInt(quantityValue.innerText) - 1;
+    if (newValue < 1) {
+      return;
+    }
+
+    quantityValue.innerText = newValue;
+  });
+}
+
+function getProductTabs() {
+  const tabs = $(".products_page__tabs");
+  const tab_anchors = tabs.querySelectorAll("a");
+  const results = [];
+
+  for (const anchor of tab_anchors) {
+    if (anchor.id.length > 0) results.push("#" + anchor.id);
+  }
+
+  return results;
+}
+
+function productPageTabs() {
+  const tabIDs = getProductTabs();
+
+  for (const id of tabIDs) {
+    const tab = $(id);
+    if (!tab) {
+      console.log("Tab with ID" + id + " not found");
+      return;
+    }
+
+    tab.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(`${id} tab clicked`);
+      activateTab(id);
+    });
+  }
+}
+
+function activateTab(tabID) {
+  const tab = $(tabID);
+  if (!tab) {
+    console.log("Tab with ID " + tabID + " not found");
+    return;
+  }
+
+  if (!tab.classList.contains("active")) {
+    console.log(`Tab ${tabID} is inactive`);
+    tab.classList.add("active");
+  }
+
+  const tabContent = $(`${tabID}__content`);
+  if (!tabContent) {
+    console.log(`Tab ${tabID}__content not found`);
+    return;
+  }
+
+  tabContent.style.display = "contents";
+  deactivateTabs(tabID);
+}
+
+function deactivateTabs(activeTabID) {
+  const allTabs = getProductTabs();
+  for (const tab of allTabs) {
+    const tabAnchor = $(tab);
+
+    if (`#${tabAnchor.id}` === activeTabID) {
+      continue;
+    }
+
+    tabAnchor.classList.remove("active");
+    const tabContent = $(`${tab}__content`);
+    tabContent.style.display = "none";
+  }
 }
--- web/project_feather/proposals.md
+++ web/project_feather/proposals.md
@@ -4,7 +4,6 @@
 
 - [ ] Rename classes `.fg-align-*` to simply `.align-*`
 - [ ] Add `.d-inline` class
-- [ ] `grid-gap` property should be separated from `.d-grid` class
 - [ ] Margin and Padding `auto` classes
   - [ ] `.m-auto` and `.p-auto`
   - [ ] `.mh-auto` and `.ph-auto`
@@ -27,3 +26,8 @@
   - [ ] Increase `--spacing-2` value to `1.8rem`
 - [ ] UL Bug: Change `--font-size-normal` to `--fg-size-normal`
 - [ ] UL Change value of property `list-style` to `inside`;
+- [ ] Grid Improvements
+  - [ ] Implement variable `--grid-gap`
+  - [ ] `grid-gap` property should be separated from `.d-grid` class
+  - [ ] Implment grid columns minimum width as variables
+- [ ] Implement following responsive properties
