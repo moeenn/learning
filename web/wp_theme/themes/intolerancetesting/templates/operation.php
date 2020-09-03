f5832d8290e1c54eec30c8e5ee7080d356eb8214
moeenn <moeen.v8@gmail.com>
2020-09-03 13:17:40 +0500

    September 03, 2020 - 01:17:40 PM

--- web/wp_theme/themes/intolerancetesting/templates/operation.php
+++ web/wp_theme/themes/intolerancetesting/templates/operation.php
@@ -48,7 +48,116 @@ get_header();
 ?>
 
 
+<!-- appropriate expectations -->
+<section class="bg-color-white">
+	<div class="container pt-3 pb-big">
+		<div class="d-grid two-col">
+			<!-- first column -->
+			<div class="pt-5">
+				<h4 class="fg-color-primary"><?php the_field('operations__checklist__col_one__heading'); ?></h4>
 
+				<div>
+					<!-- item 1 -->
+					<?php if( have_rows('operations__checklist__column_one__list')): ?>
+						<?php while( have_rows('operations__checklist__column_one__list')): the_row(); ?>
+
+							<div class="d-flex flex-h pr-5 mv-3">
+								<img
+									src="<?php echo get_sub_field('img'); ?>"
+									style="height: 2.5rem;"
+									class="mv-auto"
+								/>
+								<div class="ml-2">
+									<p class="fg-color-black fg-bold">
+										<?php echo get_sub_field('list_heading'); ?>
+									</p>
+									<p class="fg-size-small fg-color-dull">
+										<?php echo get_sub_field('list_desc'); ?>
+									</p>
+								</div>
+							</div>
+
+						<?php endwhile; ?>
+					<?php endif; ?>	
+
+				</div>
+			</div>
+
+			<!-- second column -->
+			<div class="pt-5">
+				<div>
+					<h4 class="fg-color-primary"><?php the_field('operations__checklist__col_two__heading'); ?></h4>
+					<p class="fg-color-black fg-bold mv-2">
+						<?php the_field('operations__checklist__column_two__subheading'); ?>
+					</p>
+					<div class="fg-size-small fg-color-dull">
+						<ul class="pv-1">
+							<?php if(have_rows('operations__checklist__column_two__list')): ?>
+								<?php while(have_rows('operations__checklist__column_two__list')): the_row(); ?>
+
+									<li><?php echo get_sub_field('list_item'); ?></li>
+
+								<?php endwhile; ?>
+							<?php endif; ?>
+
+						</ul>
+
+						<p class="fg-size-small">
+							<?php the_field('operations__checklist__column_two__desc'); ?>
+						</p>
+					</div>
+
+					<?php $button_link = get_field('operations__checklist__column_two__call_to_action'); ?>
+					<button class="button-primary shadow-accent mt-3">
+						<a href="<?php echo $button_link['url']; ?>"><?php echo $button_link['title']; ?></a>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section>
+	<div class="container pt-3 pb-big">
+		<div class="d-grid two-col">
+			<!-- first column -->
+			<div class="pt-5">
+				<h4 class="fg-color-primary">
+					<?php the_field('operations__details__column_one__heading'); ?>
+				</h4>
+				<p class="fg-size-small fg-color-dull pb-2 pr-big">
+					<?php the_field('operations__details__column_one__desc'); ?>					
+				</p>
+			</div>
+
+			<!-- second column -->
+			<div class="pt-5">
+				<div>
+					<h6 class="fg-color-primary">
+						<?php the_field('operations__details__column_two__heading'); ?>
+					</h6>
+					<p class="fg-size-small fg-color-dull mb-2">
+						<?php the_field('operations__details__column_two__desc'); ?>
+					</p>
+
+					<div class="fg-size-small fg-color-dull">
+						<ul class="pv-1">
+							<?php if(have_rows('operations__details__column_two__list')): ?>
+								<?php while(have_rows('operations__details__column_two__list')): the_row(); ?>
+				
+									<li><?php echo get_sub_field('list_item'); ?></li>
+
+								<?php endwhile; ?>
+							<?php endif; ?>
+			
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 
 <?php
