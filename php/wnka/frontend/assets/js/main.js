3921166d277c7ebf237e67839ebf103fbf7714d4
moeenn <moeen.v8@gmail.com>
2020-09-15 16:53:12 +0500

    September 15, 2020 - 04:53:12 PM

--- php/wnka/frontend/assets/js/main.js
+++ php/wnka/frontend/assets/js/main.js
@@ -9,6 +9,9 @@ function main() {
 
   // activate clicked link, deactivate all others
   monitorActivation(yearLinks);
+
+  const elem = document.getElementById("2020");
+  console.log("Is visible: #2020 :: ", isElementVisible(elem));
 }
 
 function getYearLinks() {
@@ -46,4 +49,30 @@ function monitorActivation(linkElements) {
       setAsActive(element);
     });
   }
+}
+
+function isElementVisible(el) {
+  var rect = el.getBoundingClientRect(),
+    vWidth = window.innerWidth || document.documentElement.clientWidth,
+    vHeight = window.innerHeight || document.documentElement.clientHeight,
+    efp = function (x, y) {
+      return document.elementFromPoint(x, y);
+    };
+
+  // Return false if it's not in the viewport
+  if (
+    rect.right < 0 ||
+    rect.bottom < 0 ||
+    rect.left > vWidth ||
+    rect.top > vHeight
+  )
+    return false;
+
+  // Return true if any of its four corners are visible
+  return (
+    el.contains(efp(rect.left, rect.top)) ||
+    el.contains(efp(rect.right, rect.top)) ||
+    el.contains(efp(rect.right, rect.bottom)) ||
+    el.contains(efp(rect.left, rect.bottom))
+  );
 }
