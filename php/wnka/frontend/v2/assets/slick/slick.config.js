48d3e9e1928b9e68084a4bc7d063bb2ee7515ee1
moeenn <moeen.v8@gmail.com>
2020-09-18 20:52:33 +0500

    September 18, 2020 - 08:52:33 PM

--- php/wnka/frontend/v2/assets/slick/slick.config.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".slider-container").slick({
-    slidesToShow: 1,
-    lazyLoad: "ondemand",
-    slidesToScroll: 1,
-    speed: 300,
-    infinite: false,
-  });
-});
