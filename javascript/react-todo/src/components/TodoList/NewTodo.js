c7a78179fde47cc026481e6f8d3f38c56937fffc
moeenn <moeen.v8@gmail.com>
2021-06-06 15:36:25 +0500

    updated hooks, cleaned code

--- javascript/react-todo/src/components/TodoList/NewTodo.js
+++ javascript/react-todo/src/components/TodoList/NewTodo.js
@@ -1,27 +1,27 @@
-import { useState } from "react";
+import { useRef } from "react";
 import styles from "./NewTodo.module.css";
 
-const NewTodo = (props) => {
-  const [item, setItem] = useState("");
+const NewTodo = ({ setTodos }) => {
+  const todoNameRef = useRef();
 
   return (
     <form
       className={styles.flex}
       onSubmit={(e) =>
-        handleSubmit(e, props.todos, props.setTodos, item)
+        handleSubmit(e, setTodos, todoNameRef)
       }
     >
       <input
         className={styles.item_input}
         type="text"
         placeholder="Add Item..."
-        onChange={(e) => handleChange(e, setItem)}
+        ref={todoNameRef}
       />
       <input
         type="submit"
         className={styles.item_submit}
         onClick={(e) =>
-          handleSubmit(e, props.todos, props.setTodos, item)
+          handleSubmit(e, setTodos, todoNameRef)
         }
         value="Add"
       />
@@ -33,21 +33,18 @@ const generate_uuid = () => {
   return Math.random().toString(36).substring(7);
 };
 
-const handleChange = (event, setItem) => {
-  const value = event.target.value;
-  setItem(value);
-};
-
-const handleSubmit = (e, todos, setTodos, item) => {
+const handleSubmit = (e, setTodos, todoNameRef) => {
   e.preventDefault();
+  const text = todoNameRef.current.value;
 
   const newItem = {
     id: generate_uuid(),
-    text: item,
     done: false,
+    text,
   };
 
-  setTodos([...todos, newItem]);
+  setTodos(prevousTodos => [...prevousTodos, newItem]);
+  todoNameRef.current.value = null;
 };
 
 export default NewTodo;
--- javascript/react-todo/src/components/TodoList/Todo.js
+++ javascript/react-todo/src/components/TodoList/Todo.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NewTodo from "./NewTodo.js";
 import TodoItem from "./TodoItem.js";
@@ -7,10 +7,31 @@ import styles from "./Todo.module.css";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const storeName = "todos";
+
+  /**
+   *  get todos from localstorage, if they exist
+   *  this will only run once because the second argument will never change
+  */
+  useEffect(() => {
+    const todosJSON = localStorage.getItem(storeName);
+    if (!todosJSON) return;
+
+    setTodos(JSON.parse(todosJSON));
+  }, []);
+
+  /**
+   *  save todos to localstorage. This hook runs every time the values in the 
+   *  second argument change
+  */
+  useEffect(() => {
+    const todosJSON = JSON.stringify(todos);
+    localStorage.setItem(storeName, todosJSON);
+  }, [todos]);
 
   return (
     <>
-      <NewTodo todos={todos} setTodos={setTodos} />
+      <NewTodo setTodos={setTodos} />
 
       <div className={styles.todo_list}>
         {todos.map((todo) => {
