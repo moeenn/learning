5371e94fa35f0a3f99694b51a84834abc82b8329
H1Y1B8S1 <h1y1b8s1@gmail.com>
2024-03-05 15:39:09 +0530

    1.1

--- assignments/01_formula_translation/code/02.cpp
+++ assignments/01_formula_translation/code/02.cpp
@@ -1,19 +1,36 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	// Draw the figure
-	cout << "      *       " << endl;
-	cout << "     ***      " << endl;
-	cout << "    *****     " << endl;
-	cout << "   *******    " << endl;
-	cout << "  *********   " << endl;
-	cout << "   *******    " << endl;
-	cout << "    *****     " << endl;
-	cout << "     ***      " << endl;
-	cout << "      *       " << endl;
+	// // Draw the figure
+	// cout << "      *       " << endl;
+	// cout << "     ***      " << endl;
+	// cout << "    *****     " << endl;
+	// cout << "   *******    " << endl;
+	// cout << "  *********   " << endl;
+	// cout << "   *******    " << endl;
+	// cout << "    *****     " << endl;
+	// cout << "     ***      " << endl;
+	// cout << "      *       " << endl;
 
-	// end program
+	int height = 9; // Adjust this value if you want a different height
+
+    for (int i = 0; i < height; ++i) {
+        // Print leading spaces
+        for (int j = 0; j < height - i - 1; ++j) {
+            std::cout << " ";
+        }
+
+        // Print asterisks
+        for (int k = 0; k < 2 * i + 1; ++k) {
+            std::cout << "*";
+        }
+
+        std::cout << std::endl;
+    }
 	return 0;
 }
+
+
+
